b5f47b551d6bb2d8a5ec567e9f8b6e3ee511391d
Ilyes Bouchlaghem <ilyesbouchlaghem@gmail.com>
2021-04-01 23:16:25 +0400

    Create samiiira_hello_world.c

---
+++ samiiira_hello_world.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  char a[6]="Hello";
+    char b[7]="World";
+    printf("%c", a[0]);
+    printf("%c", a[1]);
+    printf("%c", a[2]);
+    printf("%c", a[3]);
+    printf("%c ", a[4]);
+    printf("%c", b[0]);
+    printf("%c", b[1]);
+    printf("%c", b[2]);
+    printf("%c", b[3]);
+    printf("%c", b[4]);
+    printf("%c", b[5]);
+        return(0);
+}
