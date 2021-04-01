dc56489162364a09cd5606a2dc93e6e7d053ed4a
Marcos <81752503+Dexter42AD@users.noreply.github.com>
2021-04-01 21:31:52 +0400

    Add files via upload

---
+++ HelloWorld_Challenge_MMH.py
@@ -0,0 +1,70 @@
+"""
+Worst Hello World Challenge! 
+This is to create the worst possible Hello World! code
+"""
+
+string = "Hello "
+lookingfor = "H"
+for c in range(0, len(string)):
+    if string[c] == lookingfor:
+        print(string[c])
+
+string = "Hello "
+lookingfor = "e"
+for c in range(0, len(string)):
+    if string[c] == lookingfor:
+        print(string[c])
+
+string = "Hello "
+lookingfor = "l"
+for c in range(0, len(string)):
+    if string[c] == lookingfor:
+        print(string[c])
+
+string = "Hello "
+lookingfor = "o"
+for c in range(0, len(string)):
+    if string[c] == lookingfor:
+        print(string[c])
+
+string = "Hello "
+lookingfor = " "
+for c in range(0, len(string)):
+    if string[c] == lookingfor:
+        print(string[c])
+
+string = "world!"
+lookingfor = "w"
+for c in range(0, len(string)):
+    if string[c] == lookingfor:
+        print(string[c])
+
+string = "world!"
+lookingfor = "o"
+for c in range(0, len(string)):
+    if string[c] == lookingfor:
+        print(string[c])
+
+string = "world!"
+lookingfor = "r"
+for c in range(0, len(string)):
+    if string[c] == lookingfor:
+        print(string[c])
+
+string = "world!"
+lookingfor = "l"
+for c in range(0, len(string)):
+    if string[c] == lookingfor:
+        print(string[c])
+
+string = "world!"
+lookingfor = "d"
+for c in range(0, len(string)):
+    if string[c] == lookingfor:
+        print(string[c])
+
+string = "world!"
+lookingfor = "!"
+for c in range(0, len(string)):
+    if string[c] == lookingfor:
+        print(string[c])
