e48b1520a901f39b024ac44fe70f6f7f1c7331b7
Ilyes Bouchlaghem <ilyesbouchlaghem@gmail.com>
2021-04-05 14:39:59 +0400

    Update samer_hw.c

--- samer_hw.c
+++ samer_hw.c
@@ -1,113 +1,265 @@
-/****************************************************************************
-This outputs 'Hello World'
+/*************************************************************************************
+This program outputs 'Hello World\n'
 @author Samer Saber
-****************************************************************************/
+**************************************************************************************/
 #include <stdio.h>
 
-#define OP_ASSGN   0x1
+#define OP_LOAD		0x0
+#define OP_ADD		0x1
+#define OP_SUB		0x2
+#define OP_AND		0x3
+#define OP_OR		0x4
+#define OP_SH_LEFT	0x5
+#define OP_SH_RIGHT	0x6
+#define OP_HALT		0x7
 
-#define ASSGN(Dest, Address)       (OP_ASSGN  << 9) | (Address << 3)  | Dest
+#define LOAD(Reg1, Reg2)		OP_LOAD << 12 | Reg1 << 8 | Reg2 << 4
+#define ADD(Reg1, Reg2, Reg3)	OP_ADD  << 12 | Reg1 << 8 | Reg2 << 4 | Reg3
+#define SUB(Reg1, Reg2, Reg3)	OP_SUB  << 12 | Reg1 << 8 | Reg2 << 4 | Reg3
+#define AND(Reg1, Reg2, Reg3)	OP_AND  << 12 | Reg1 << 8 | Reg2 << 4 | Reg3
+#define OR(Reg1, Reg2, Reg3)	OP_OR   << 12 | Reg1 << 8 | Reg2 << 4 | Reg3
+#define SH_LEFT(Reg1, shifts)	OP_SH_LEFT << 12 | Reg1 << 8
+#define SH_RIGHT(Reg1, shifts)	OP_SH_RIGHT << 12 | Reg1 << 8
+#define HALT 					OP_HALT << 12
 
-#define L0   0x0
-#define L1   0x1
-#define L2   0x2
-#define L3   0x3
-#define L4   0x4
-#define L5   0x5
-#define L6   0x6
-#define L7   0x7
+#define REG0		0x0
+#define REG1		0x1
+#define REG2 		0x2
+#define REG3 		0x3
+#define REG4 		0x4
+#define REG5 		0x5
+#define REG6 		0x6
+#define REG7 		0x7
+#define REG8 		0x8
+#define REG9 		0x9
+#define REG10 		0xA
+#define REG11 		0xB
+#define REG12 		0xC
+#define REG13 		0xD
 
-unsigned char L[12];
+unsigned char registers[42];
 
-unsigned short IM[12] =
+unsigned short DM[42] =
 {
-    ASSGN(L0,2),
-    ASSGN(L1,5),
-    ASSGN(L2,6),
-    ASSGN(L3,6),
-    ASSGN(L4,7),
-    ASSGN(L5,1),
-    ASSGN(L6,3),
-    ASSGN(L7,7),
+	10,
+	42,
+	30,
+	202,
+	200,
+	92,
+	54,
+	222,
+	36,
+	59,
+	17,
+	70,
+	90,
+	24,
+	213,
+	113
+};
+
+unsigned short IM[42] = 
+{
+	LOAD(REG12,1),
+	LOAD(REG13,2),
+	ADD(REG0,REG12,REG13),
+	LOAD(REG1,3),
+	SH_RIGHT(REG1,1),
+	LOAD(REG12,4),
+	LOAD(REG13,5),
+	SUB(REG2,REG12,REG13),
+	LOAD(REG3,6),
+	SH_LEFT(REG3,2),
+	LOAD(REG4,7),
+	SH_RIGHT(REG4,1),
+	LOAD(REG12,8),
+	LOAD(REG13,9),
+	AND(REG5,REG12,REG13),
+	LOAD(REG12,10),
+	LOAD(REG13,11),
+	OR(REG6,REG12,REG13),
+	LOAD(REG7,7),
+	SH_RIGHT(REG7,5),
+	LOAD(REG12,12),
+	LOAD(REG13,13),
+	ADD(REG8,REG12,REG13),
+	LOAD(REG12,4),
+	LOAD(REG13,5),
+	SUB(REG9,REG12,REG13),
+	LOAD(REG12,14),
+	LOAD(REG13,15),
+	SUB(REG10,REG12,REG13),
+	LOAD(REG11,0),
+	HALT
 };
 
 
-unsigned short DM[9] =
-{
-    10,
-    32,
-    72,
-    87,
-    100,
-    101,
-    108,
-    111,
-    114
-};
+void load(unsigned short reg1, unsigned short reg2);
+void add(unsigned short reg1, unsigned short reg2, unsigned short reg3);
+void sub(unsigned short reg1, unsigned short reg2, unsigned short reg3);
+void and(unsigned short reg1, unsigned short reg2, unsigned short reg3);
+void or(unsigned short reg1, unsigned short reg2, unsigned short reg3);
+void shiftLeft(unsigned short reg1, unsigned short reg2);
+void shiftRight(unsigned short reg1, unsigned short reg2);
+void halt();
 
-unsigned short opCount  = 0;
+void instructionDecodeUnit(unsigned short *opCode, unsigned short *reg1, unsigned short *reg2, unsigned short *reg3);
+
+unsigned short IC = 0;
+
+void incIC();
 
 unsigned short run = 1;
 
-unsigned short InstructionDecodeUnit(unsigned short uInstr, unsigned short *uDest, unsigned short *uReg1, unsigned short *uReg2);
 
-void assgn(unsigned short uReg, unsigned short uAddress);
-void stop(void);
-
-void incrOpCount(void);
 
 int main()
 {
-    unsigned short uReg1  = 0, uReg2  = 0, uDest = 0, uOpCode = 0;
 
-    while(run)
-    {
-        uOpCode = InstructionDecodeUnit(IM[opCount], &uDest, &uReg1, &uReg2);
+	unsigned short opCode = 0, reg1 = 0, reg2 = 0, reg3 = 0;
 
-        switch ( uOpCode)
-        {
-            case OP_ASSGN:
-                assgn(uReg1, uDest);
-                break;
+	while(run)
+	{
+		instructionDecodeUnit(&opCode, &reg1, &reg2, &reg3);
 
-            default:
-                stop();
-                break;
-        }
+		switch(opCode)
+		{
+			case OP_LOAD:
+				load(reg1,reg2);
+				incIC();
+				break;
 
-        printf("%c", L[opCount]);
-        incrOpCount();
-    }
+			case OP_ADD:
+				add(reg1,reg2,reg3);
+				incIC();
+				break;
 
-    printf("rld\n");
+			case OP_SUB:
+				sub(reg1,reg2,reg3);
+				incIC();
+				break;
 
-    return (0);
+			case OP_AND:
+				and(reg1,reg2,reg3);
+				incIC();
+				break;
+
+			case OP_OR:
+				or(reg1,reg2,reg3);
+				incIC();
+				break;
+
+			case OP_SH_LEFT:
+				shiftLeft(reg1,reg2);
+				incIC();
+				break;
+
+			case OP_SH_RIGHT:
+				shiftRight(reg1,reg2);
+				incIC();
+				break;
+
+			case OP_HALT:
+				halt();
+				break;
+
+			default:
+				printf("Error\n");
+				halt();
+		}
+	}
+
+	for (int i = 0; i < 12; i++)
+		{
+			printf("%c", registers[i]);
+		}
+
+
+	return 0;
 }
 
-unsigned short InstructionDecodeUnit(unsigned short uInstruction, unsigned short *uDest, unsigned short *uReg1, unsigned short *uReg2)
+void load(unsigned short reg1, unsigned short reg2)
 {
-    unsigned short uOpCode = (uInstruction >> 9) & 0x7F;
+	unsigned short temp = IC;
 
-    *uDest = (uInstruction & 0x7);
-    *uReg1 = (uInstruction >> 3) & 0x7;
-    *uReg2 = 1;
+	registers[reg1] = DM[reg2];
 
-    return(uOpCode);
-
+	IC = temp;
 }
 
-
-void incrOpCount()
+void add(unsigned short reg1, unsigned short reg2, unsigned short reg3)
 {
-   opCount = opCount + 1;
+	registers[reg1] = registers[reg2] + registers[reg3];
 }
 
-void assgn(unsigned short uAddress, unsigned short uReg)
+void sub(unsigned short reg1, unsigned short reg2, unsigned short reg3)
 {
-    L[uReg] = DM[uAddress];
+	registers[reg1] = registers[reg2] - registers[reg3];
 }
 
-void stop()
+void and(unsigned short reg1, unsigned short reg2, unsigned short reg3)
 {
-    run = 0;
+	registers[reg1] = registers[reg2] & registers[reg3];
+}
+
+void or(unsigned short reg1, unsigned short reg2, unsigned short reg3)
+{
+	registers[reg1] = registers[reg2] | registers[reg3];
+}
+
+void shiftLeft(unsigned short reg1, unsigned short reg2)
+{
+	registers[reg1] = registers[reg1] << reg2;
+}
+
+void shiftRight(unsigned short reg1, unsigned short reg2)
+{
+	registers[reg1] = registers[reg1] >> reg2;
+}
+
+void halt()
+{
+	run = 0;
+}
+
+void instructionDecodeUnit(unsigned short *opCode, unsigned short *reg1, unsigned short *reg2, unsigned short *reg3)
+{
+	*opCode = IM[IC] >> 12;
+
+	switch(*opCode)
+	{
+		case OP_LOAD:
+			*reg1 = (IM[IC] >> 8) & 0XF;
+			*reg2 = (IM[IC] >> 4) & 0XF;
+			*reg3 = 1;
+			break;
+
+		case OP_ADD:
+		case OP_SUB:
+		case OP_AND:
+		case OP_OR:
+			*reg1 = (IM[IC] >> 8) & 0XF;
+			*reg2 = (IM[IC] >> 4) & 0XF;
+			*reg3 =  IM[IC]       & 0XF;
+			break;
+
+		case OP_SH_LEFT:
+		case OP_SH_RIGHT:
+			*reg1 = (IM[IC] >> 8) & 0XF;
+			*reg2 = 1;
+			*reg3 = 1;
+			break;
+
+		case OP_HALT:
+			break;
+
+		default:
+			printf("Error\n");
+	}
+}
+
+void incIC()
+{
+	IC++;
 }
